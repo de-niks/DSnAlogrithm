f1209f380ddcc38687a97aef1ecf673023cdec32
de-niks <de.niks.tech@gmail.com>
2024-10-21 18:19:42 -0400

    LC1474 Delete M_Nodes After N Nodes

---
+++ LinkedLists/LC1474_Delete_M_Nodes_After_N_Nodes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    internal class LC1474_Delete_M_Nodes_After_N_Nodes
+    {
+        public ListNode DeleteNodes(ListNode head, int m, int n)
+        { 
+            var sentinel = new ListNode(Int32.MinValue, head);
+
+            var current = head;
+
+            ListNode predecessor = sentinel;
+
+            while (current != null)
+            {
+                int index = 1;
+
+                while (current != null && index <= m)
+                {
+                    predecessor = current;  
+                    current =   current.next;
+                    index++;
+                }
+
+                if(current == null)
+                    break;
+
+
+                index = 1;
+
+                var messanger = current;
+                while (messanger != null && index <= n)
+                { 
+                    messanger = messanger.next; 
+                    index++;
+                }
+
+                predecessor.next = messanger;
+                current = messanger;
+
+            }
+
+
+            return sentinel.next;
+        }
+    }
+}
--- LinkedLists/Program.cs
+++ LinkedLists/Program.cs
@@ -5,8 +5,17 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        LC82_Remove_Duplicates_from_Sorted_List_II obj = new LC82_Remove_Duplicates_from_Sorted_List_II();
-        ListNode node = new ListNode(1, new ListNode(1, null));
-        obj.DeleteDuplicates(node);
+        //LC82_Remove_Duplicates_from_Sorted_List_II obj = new LC82_Remove_Duplicates_from_Sorted_List_II();
+        //ListNode node = new ListNode(1, new ListNode(1, null));
+        //obj.DeleteDuplicates(node);
+
+       
+
+        ListNode node1 = new ListNode(1, new ListNode(2, new ListNode(3,new ListNode(4,new ListNode(5,new ListNode(6, new ListNode(7,new ListNode(8, new ListNode(9,new ListNode(10,new ListNode(11, null)))))))))));
+
+        LC1474_Delete_M_Nodes_After_N_Nodes lC1474_Delete_M_Nodes_After_N_Nodes = new LC1474_Delete_M_Nodes_After_N_Nodes();
+        lC1474_Delete_M_Nodes_After_N_Nodes.DeleteNodes(node1, 3, 1);
+
+
     }
 }
