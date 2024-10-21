9bf533c0b2215f2a98ac389d128c2b3310b8c585
de-niks <de.niks.tech@gmail.com>
2024-10-21 17:18:26 -0400

    Remove Nth From End Medium LC

---
+++ LinkedLists/LC19_RemoveNthFromEnd_List.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    internal class LC19_RemoveNthFromEnd_List
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var sentinel = new ListNode(Int32.MinValue , head);
+
+            var leader = sentinel;
+
+            //move the leader Nth nodes ahead
+            for (int i = 0; i < n; i++)
+                leader = leader.next;
+
+            var follower = sentinel;
+
+            ListNode predecessor = null;
+
+            while (leader != null) { 
+
+                leader = leader.next;
+                predecessor = follower;
+                follower = follower.next;
+            
+            }
+            // Assign predecessor to next of follower which is node to be deleted
+            //Follower is the node to be deleted as it is Nth steps behind leader
+            predecessor.next = follower.next;
+
+            head = sentinel.next;
+
+            return head;
+        }
+    }
+}
