d874933cb333cc32dd530850f1438b38732148e0
de-niks <de.niks.tech@gmail.com>
2024-10-28 19:58:59 -0400

    SwapNodes in LL with Kth element

---
+++ LinkedLists/SwapNodes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    internal class SwapNodes
+    {
+        public ListNode SwapNodesLL(ListNode head, int k)
+        {
+            var current = head;
+
+            int index = 1;
+            while (index < k)
+            {
+                current = current.next;
+                index++;
+            }
+
+            var tempNode = current;
+
+            var hare = current;
+            var tortoise = head;
+            while (hare != null && hare.next != null)
+            {
+                hare = hare.next;
+                tortoise = tortoise.next;
+            }
+            var temp = tempNode.val;
+            tempNode.val = tortoise.val;
+            tortoise.val = temp;
+
+
+            return head;
+
+        }
+    }
+}
