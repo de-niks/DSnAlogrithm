6dd44778ea56a3401ef518ca6a3b841dcd35b3d1
de-niks <de.niks.tech@gmail.com>
2024-10-28 17:31:33 -0400

    ReverseLL II

--- LinkedLists/ReverseLinkedList.cs
+++ LinkedLists/ReverseLinkedList.cs
@@ -57,5 +57,75 @@ namespace LinkedLists
             return predecessor;
 
         }
+
+        public ListNode ReverseInterativeII(ListNode head, int left, int right)
+        {
+            var sentinelNode = new ListNode(int.MinValue, head);
+
+            var current = head;
+            int index = 1;
+            var predecessor = sentinelNode;
+            while (index != left)
+            {
+                predecessor = current;
+
+                current = current.next;
+
+                index++;
+            }
+
+            // cut the link before sub list starts 
+            predecessor.next = null;
+
+            var tail_left = predecessor;
+            predecessor = null;
+
+            var tail_middle = current;
+
+            //keep rversing the sublist
+            while (index != right + 1)
+            {
+                ListNode successor = current.next;
+                current.next = predecessor;
+                predecessor = current;
+                current = successor;
+                index++;
+            }
+
+            var head_middle = predecessor;
+            var head_right = current;
+
+            tail_left.next = head_middle;
+            tail_middle.next = head_right;
+
+
+            return sentinelNode.next;
+
+
+            /* if (head == null || left == right) 
+                 return head;
+
+             ListNode dummy = new ListNode(0);
+             dummy.next = head;
+             ListNode prev = dummy;
+
+             for (int i = 0; i < left - 1; ++i)
+             {
+                 prev = prev.next;
+             }
+
+             ListNode current = prev.next;
+
+             for (int i = 0; i < right - left; ++i)
+             {
+                 ListNode nextNode = current.next;
+                 current.next = nextNode.next;
+                 nextNode.next = prev.next;
+                 prev.next = nextNode;
+             }
+
+             return dummy.next;*/
+
+        }
     }
 }
