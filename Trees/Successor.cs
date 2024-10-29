35df3548d84c7745a64e64c59fd67d2175ef5f04
de-niks <de.niks.tech@gmail.com>
2024-10-29 06:40:30 -0400

    Successor of a tree node

---
+++ Trees/Successor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    internal class Successor
+    {
+        public TreeNode FindSuccessor(TreeNode root, TreeNode p)
+        {
+            if (root == null)
+                return null;
+
+            /// if node has right subtree
+            /// successor would be in the left sub tree
+            /// of the node's right
+            if (p.right != null)
+            {
+                var current = p.right;
+
+                ///keep going to left 
+                while (current.left != null)
+                {
+                    current = current.left;
+                }
+                ///return minimum in left 
+                ///subtree which is successor
+                return current;
+            }
+
+
+            TreeNode anscestor = null;
+            
+            var curr = root;
+            while (curr.Key != p.Key)
+            {
+                if (p.Key < curr.Key)
+                {
+                    anscestor = curr;
+                    curr = curr.left;
+                }
+                else
+                {
+                    curr = curr.right;
+                }
+            }
+
+            return curr;
+
+
+        }
+    }
+}
