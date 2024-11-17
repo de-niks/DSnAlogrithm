1b5c914d6712e7cc668058a444e324bd1857b866
de-niks <de.niks.tech@gmail.com>
2024-11-17 07:06:12 -0500

    Greedy Intution but failed

---
+++ Greedy/NumberOfCoins.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greedy
+{
+    internal class NumberOfCoins
+    {
+        /// <summary>
+        /// Greedy approach fails here 
+        /// </summary>
+        /// <param name="coins"></param>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        public int CoinChange(int[] coins, int amount)
+        {
+            int numberOfCoins = 0;
+            int remainAmount = amount;
+           
+            foreach (int coin in coins)
+            {
+                var quotient = remainAmount/coin;
+                numberOfCoins += quotient;
+                remainAmount = remainAmount % coin;
+            }
+
+            return numberOfCoins;
+        }
+    }
+
+
+}
